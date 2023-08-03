c60ac90782af570dc3d139cfb55ae2073bc97182
Habba002 <habib.bushra002@gmail.com>
2023-08-03 10:35:39 +0300

    boilerplate for http server complete

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/habibbushira/rssaggregator
+
+go 1.18
+
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT is not find in the environment variable")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handleReadiness)
+	v1Router.Get("/err", handleErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server starting on port %s", port)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
